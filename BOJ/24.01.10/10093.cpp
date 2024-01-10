ac90471ceb1d0ec7ce6d67e819e496d44220f0fc
Rylah <minsonyo@naver.com>
2024-01-10 14:40:17 +0900

    Create 10093.cpp

---
+++ BOJ/24.01.10/10093.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	long long a, b;
+	cin >> a >> b;
+
+	if (a > b)
+		swap(a, b);
+	int cnt = 0;
+	for (long long i = a + 1; i < b; i++)
+		cnt++;
+
+	cout << cnt << "\n";
+	for (long long i = a + 1; i < b; i++)
+		cout << i << " ";
+
+	
+	return 0;
+}
