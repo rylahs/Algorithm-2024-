a015b619117dd5f5e4742381eeed610886913b47
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-19 22:49:12 +0900

    Create 25305.cpp

---
+++ BOJ/24.01.19/25305.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, k;
+	cin >> n >> k;
+
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end(), greater<>());
+
+	cout << v[k - 1] << "\n";
+
+	return 0;
+}
