8f73ee55176a0c9b6e7e11c83421fa80c90d728e
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-31 23:57:17 +0900

    Create 11654.cpp

---
+++ BOJ/24.01.31/11654.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	char a;
+	cin >> a;
+
+	cout << int(a) << "\n";
+
+	return 0;
+}
