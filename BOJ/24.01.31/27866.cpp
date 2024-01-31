8b0ea072b384472bcaed16cfee4716080e9cfae2
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-31 23:59:34 +0900

    Create 27866.cpp

---
+++ BOJ/24.01.31/27866.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string s;
+	int n;
+	cin >> s >> n;
+
+	cout << s[n - 1] << "\n";
+
+	return 0;
+}
