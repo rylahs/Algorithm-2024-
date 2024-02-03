388e0a0fcc45b138aa0aac58d25936139e751617
Rylah <94848571+rylahs@users.noreply.github.com>
2024-02-03 22:24:40 +0900

    Create 11718.cpp

---
+++ BOJ/24.02/24.02.03/11718.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	while (!cin.eof())
+	{
+		string str;
+		getline(cin, str);
+
+		cout << str << "\n";
+	}
+
+	return 0;
+}
