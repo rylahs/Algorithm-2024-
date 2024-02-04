780fc4b53a82e4fdd6f6bbe1ad130a8e58c111a0
Rylah <94848571+rylahs@users.noreply.github.com>
2024-02-04 11:02:54 +0900

    Create 3046.cpp

---
+++ BOJ/24.02/24.02.04/3046.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int r1, s;
+	cin >> r1 >> s;
+
+	int r2 = s * 2 - r1;
+	cout << r2 << "\n";
+
+	return 0;
+}
