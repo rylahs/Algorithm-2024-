5ac13cd27bb9fcd0d7c8ef5a07f55d6403bc3164
Rylah <94848571+rylahs@users.noreply.github.com>
2024-02-06 18:32:42 +0900

    Create 7287.cpp

---
+++ BOJ/24.02/24.02.06/7287.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cout << 135 << "\n";
+	cout << "mintarisha" << "\n";
+
+	return 0;
+}
