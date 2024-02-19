03c25066631ca3f0931990a0d6cbc34b7090f893
Rylah <94848571+rylahs@users.noreply.github.com>
2024-02-20 00:07:28 +0900

    Create 174704.cpp

---
+++ Goorm/Level 1/174704.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	double a, b;
+	cin >> a >> b;
+
+	double res = a + b;
+	cout << fixed;
+	cout.precision(6);
+	cout << res << "\n";
+
+	return 0;
+}
