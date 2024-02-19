a0336d3016d44e5ccca3c0b91a0ae87c5bf4403b
Rylah <94848571+rylahs@users.noreply.github.com>
2024-02-19 15:18:34 +0900

    Create 1463.cpp

---
+++ BOJ/24.02/24.02.19/1463.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[1000005];
+
+void run(int n)
+{
+	dp[1] = 0;
+
+	for (int i = 2; i <= n; i++)
+	{
+		dp[i] = dp[i - 1] + 1;
+		if (i % 2 == 0)
+			dp[i] = min(dp[i], dp[i / 2] + 1);
+		if (i % 3 == 0)
+			dp[i] = min(dp[i], dp[i / 3] + 1);
+
+	}
+
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	
+	run(n);
+
+	cout << dp[n] << "\n";
+	return 0;
+}
