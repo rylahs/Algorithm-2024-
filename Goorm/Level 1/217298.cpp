e1755fb413cc3641a9335aa57a63d6687d9b37b1
Rylah <94848571+rylahs@users.noreply.github.com>
2024-02-21 16:57:19 +0900

    Create 217298.cpp

---
+++ Goorm/Level 1/217298.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int a, b;
+	cin >> a >> b;
+
+	int sum = a + b;
+	cout << sum << '\n';
+
+	return 0;
+}
