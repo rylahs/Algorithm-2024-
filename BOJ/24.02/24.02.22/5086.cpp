0bccd69a4dbdd7d73d561c01d28bcaedb2bd275f
Rylah <94848571+rylahs@users.noreply.github.com>
2024-02-22 23:42:21 +0900

    Create 5086.cpp

---
+++ BOJ/24.02/24.02.22/5086.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	while (true)
+	{
+		int a, b;
+		cin >> a >> b;
+		if (a == 0 && b == 0)
+			break;
+
+		if (b % a == 0)
+			cout << "factor\n";
+		else if (a % b == 0)
+			cout << "multiple\n";
+		else
+			cout << "neither\n";
+	}
+
+	return 0;
+}
