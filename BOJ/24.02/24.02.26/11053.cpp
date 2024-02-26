ecdc79d0f69f79572a2546b0ba5ca22addad0d68
Rylah <94848571+rylahs@users.noreply.github.com>
2024-02-26 21:29:56 +0900

    Create 11053.cpp

---
+++ BOJ/24.02/24.02.26/11053.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[1003];
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
+
+	fill(dp, dp + n, 1);
+	
+
+	for (int i = 1; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (v[j] < v[i])
+				dp[i] = max(dp[i], dp[j] + 1);
+		}
+	}
+	
+	cout << *max_element(dp, dp + n) << "\n";
+	return 0;
+}
