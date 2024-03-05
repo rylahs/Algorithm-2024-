23fdade89b8f4f2456471c3178eeadf6e5e7d73a
Rylah <94848571+rylahs@users.noreply.github.com>
2024-03-06 00:06:33 +0900

    Create 2743.cpp

---
+++ BOJ/24.03/24.03.05/2743.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string str;
+	cin >> str;
+
+	cout << str.length() << '\n';
+
+	return 0;
+}
