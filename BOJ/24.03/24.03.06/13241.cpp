db3bc5e630a0c3ec160533a1dc0c7f77c1083377
Rylah <94848571+rylahs@users.noreply.github.com>
2024-03-06 22:01:02 +0900

    Create 13241.cpp

---
+++ BOJ/24.03/24.03.06/13241.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	long long a, b;
+	cin >> a >> b;
+	cout << lcm(a, b) << '\n';
+	
+	return 0;
+}
