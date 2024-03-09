da63bf2790aea967fa05f4d930b983f15b504a5d
Rylah <94848571+rylahs@users.noreply.github.com>
2024-03-10 01:14:01 +0900

    Create 5338.cpp

---
+++ BOJ/24.03/24.03.09/5338.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cout << "       _.-;;-._\n";
+	cout << "'-..-'|   ||   |\n";
+	cout << "'-..-'|_.-;;-._|\n";
+	cout << "'-..-'|   ||   |\n";
+	cout << "'-..-'|_.-''-._|\n";
+
+
+	return 0;
+}
