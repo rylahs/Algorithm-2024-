503493a4de8807c123dca590dc4c33a3e197161a
Rylah <94848571+rylahs@users.noreply.github.com>
2024-03-11 17:12:32 +0900

    Create 2372.cpp

---
+++ BOJ/24.03/24.03.10/2372.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cout << "Animal      Count\n";
+	cout << "-----------------\n";
+	cout << "Chickens      100\n";
+	cout << "Clydesdales     5\n";
+	cout << "Cows           40\n";
+	cout << "Goats          22\n";
+	cout << "Steers          2\n";
+	
+	return 0;
+}
