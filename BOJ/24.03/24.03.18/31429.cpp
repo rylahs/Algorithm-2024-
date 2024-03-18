4e53e2a9840fe02556c5a832fc08eb7b78c4bd7f
Rylah <94848571+rylahs@users.noreply.github.com>
2024-03-18 20:35:03 +0900

    Create 31429.cpp

---
+++ BOJ/24.03/24.03.18/31429.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+	switch (n)
+	{
+	case 1:
+		cout << 12 << ' ' << 1600 << '\n';
+		break;
+	case 2:
+		cout << 11 << ' ' << 894 << '\n';
+		break;
+	case 3:
+		cout << 11 << ' ' << 1327 << '\n';
+		break;
+	case 4:
+		cout << 10 << ' ' << 1311 << '\n';
+		break;
+	case 5:
+		cout << 9 << ' ' << 1004 << '\n';
+		break;
+	case 6:
+		cout << 9 << ' ' << 1178 << '\n';
+		break;
+	case 7:
+		cout << 9 << ' ' << 1357 << '\n';
+		break;
+	case 8:
+		cout << 8 << ' ' << 837 << '\n';
+		break;
+	case 9:
+		cout << 7 << ' ' << 1055 << '\n';
+		break;
+	case 10:
+		cout << 6 << ' ' << 556 << '\n';
+		break;
+	case 11:
+		cout << 6 << ' ' << 773 << '\n';
+		break;
+	}
+
+	return 0;
+}
