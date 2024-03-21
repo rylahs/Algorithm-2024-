f587e2dedc927f2cc118bff47b651b3dac226ff7
Rylah <94848571+rylahs@users.noreply.github.com>
2024-03-21 19:13:07 +0900

    Create 31606.cpp

---
+++ BOJ/24.03/24.03.21/31606.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int X, Y;
+    cin >> X >> Y;
+    cout << X + Y + 3 << '\n';
+
+    return 0;
+}
