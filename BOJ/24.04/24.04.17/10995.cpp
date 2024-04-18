2707db5d686e430ad141fb494569da484f78b51f
Rylah <94848571+rylahs@users.noreply.github.com>
2024-04-18 09:30:24 +0900

    Create 10995.cpp

---
+++ BOJ/24.04/24.04.17/10995.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (i % 2 == 1)
+			{
+				cout << " *";
+			}
+			else
+			{
+				cout << "* ";
+			}
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
