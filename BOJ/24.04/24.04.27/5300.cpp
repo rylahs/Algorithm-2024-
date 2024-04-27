198e613e2bddce764b678e9898539d0351edfcca
Rylah <94848571+rylahs@users.noreply.github.com>
2024-04-27 13:27:55 +0900

    Create 5300.cpp

---
+++ BOJ/24.04/24.04.27/5300.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; 
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cout << i << ' ';
+		if (i % 6 == 0)
+			cout << "Go! ";
+	}
+	if (n % 6 != 0)
+		cout << "Go!\n";
+
+
+	return 0;
+}
