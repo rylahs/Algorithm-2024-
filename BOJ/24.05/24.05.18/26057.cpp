b2008585d902827a5a8b5a75b42d7e4c37579e1c
Rylah <94848571+rylahs@users.noreply.github.com>
2024-05-18 23:17:09 +0900

    Create 26057.cpp

---
+++ BOJ/24.05/24.05.18/26057.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int l, t;
+	cin >> l >> t;
+
+	cout << 2 * t - l << '\n';
+
+	return 0;
+}
